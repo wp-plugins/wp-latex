af9070be8d840d95c124bfddf1de7f5873ad19f1
mdawaffe <mdawaffe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-05-04 21:23:39 +0000

    WP-LaTeX initial checkin
    git-svn-id: https://plugins.svn.wordpress.org/wp-latex/trunk@12195 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ automattic-latex.php
@@ -0,0 +1,247 @@
+<?php
+/*
+Inspired in part by:
+	Steve Mayer ( http://www.mayer.dial.pipex.com/tex.htm ): LatexRender WP Plugin ( http://sixthform.info/steve/wordpress/index.php )
+	Benjamin Zeiss ( zeiss@math.uni-goettingen.de ): LaTeX Rendering Class
+*/
+
+if ( !defined('AUTOMATTIC_LATEX_LATEX_PATH') || !defined('AUTOMATTIC_LATEX_DVIPNG_PATH') )
+	return;
+
+class Automattic_Latex {
+	var $_blacklist = array(
+		'^^',
+		'afterassignment',
+		'aftergroup',
+		'batchmode',
+		'catcode',
+		'closein',
+		'closeout',
+		'command',
+		'csname',
+		'document',
+		'def',
+		'errhelp',
+		'errcontextlines',
+		'errorstopmode',
+		'every',
+		'expandafter',
+		'immediate',
+		'include',
+		'input',
+		'jobname',
+		'loop',
+		'lowercase',
+		'makeat',
+		'meaning',
+		'message',
+		'name',
+		'newhelp',
+		'noexpand',
+		'nonstopmode',
+		'open',
+		'output',
+		'pagestyle',
+		'package',
+		'pathname',
+		'read',
+		'relax',
+		'repeat',
+		'shipout',
+		'show',
+		'scrollmode',
+		'special',
+		'syscall',
+		'toks',
+		'tracing',
+		'typeout',
+		'typein',
+		'uppercase',
+		'write'
+	);
+
+	var $file_base;
+	var $latex;
+	var $bg_hex;
+	var $fg_hex;
+	var $bg_rgb;
+	var $fg_rgb;
+	var $size;
+
+	function create_png( $file_base, $latex, $bg_hex = 'ffffff', $fg_hex = '000000', $size = 0 ) {
+		$object = new Automattic_Latex();
+
+		$object->init( $file_base, $latex, $bg_hex, $fg_hex, $size );
+		return $object->save_png();
+	}
+
+	function display_png( $image ) {
+		if ( @get_resource_type( $image ) ); // [sic]
+		elseif ( is_wp_error($image) )
+			$image = Automattic_Latex::render_error( $image );
+		elseif ( !file_exists($image) )
+			$image = Automattic_Latex::render_error( __( 'Error Loading Image', 'automattic-latex' ) );
+		else
+			$image = imagecreatefrompng( $image );
+
+		header("Content-Type: image/png");
+		header("Vary: Accept-Encoding"); // Handle proxies
+		header("Expires: " . gmdate("D, d M Y H:i:s", time() + 864000) . " GMT"); // 10 days
+
+		imagepng($image);
+		imagedestroy($image);
+		exit;
+	}
+
+	function init( $file_base, $latex, $bg_hex = 'ffffff', $fg_hex = '000000', $size = 0 ) {
+		$this->file_base = (string) $file_base;
+		$this->latex  = (string) $latex;
+
+		$bg_hex = (string) $bg_hex;
+		$this->bg_rgb = $this->hex2rgb( $bg_hex ? $bg_hex : 'ffffff' );
+
+		$fg_hex = (string) $fg_hex;
+		$this->fg_rgb = $this->hex2rgb( $fg_hex ? $fg_hex : '000000' );
+
+		$this->size = $this->size_it( $size );
+	}
+
+	function hex2rgb( $color ) {
+		$color = (string) $color;
+		$color = substr(preg_replace('/[^0-9a-f]/i', '', $color), 0, 6);
+		if ( 6 > $l = strlen($color) )
+			$color .= str_repeat('0', 6 - $l );
+
+		$red   = $color{0} . $color{1};
+		$green = $color{2} . $color{3};
+		$blue  = $color{4} . $color{5};
+
+		foreach ( array('red', 'green', 'blue') as $color )
+			$$color = number_format( hexdec(ltrim($$color,'0')) / 255, 3 );
+
+		return "$red $green $blue";
+	}
+
+	function size_it( $z ) {
+		switch ( (int) $z ) :
+		case 1 :
+			return 'large';
+			break;
+		case 2 :
+			return 'Large';
+			break;
+		case 3 :
+			return 'LARGE';
+			break;
+		case 4 :
+			return 'huge';
+			break;
+		case -1 :
+			return 'small';
+			break;
+		case -2 :
+			return 'footnotesize';
+			break;
+		case -3 :
+			return 'scriptsize';
+			break;
+		case -4 :
+			return 'tiny';
+			break;
+		default :
+			return false;
+			break;
+		endswitch;
+	}
+
+	function save_png() {
+		if ( !$this->latex )
+			return new WP_Error( 'automattic-latex', __( 'No formula provided', 'automattic-latex' ) );
+
+		foreach ( $this->_blacklist as $bad )
+			if ( stristr($this->latex, $bad) )
+				return new WP_Error( 'automattic-latex', __( 'Formula Invalid', 'automattic-latex' ) );
+
+		if ( preg_match('/(^|[^\\\\])\$/', $this->latex) )
+			return new WP_Error( 'automattic-latex', __( 'You must stay in inline math mode', 'automattic-latex' ) );
+
+		if ( 2000 < strlen($latex) )
+			return new WP_Error( 'automattic-latex', __( 'The formula is too long', 'automattic-latex' ) );
+
+		$latex = $this->wrap( $this->latex );
+
+		$umask = umask(0);
+			$dir = dirname($this->file_base);
+			if ( !is_dir($dir) )
+				mkdir($dir, fileperms(dirname($dir)) % 010000 );
+
+			if ( !$f = @fopen( "$this->file_base.tex", 'w' ) )
+				return new WP_Error( 'automattic-latex', __( 'Could not open TEX file for writing', 'automattic-latex' ) );
+			if ( false === @fwrite($f, $latex) )
+				return new WP_Error( 'automattic-latex', __( 'Could not write to TEX file', 'automattic-latex' ) );
+			fclose($f);
+		umask($umask);
+
+		exec( "TEXMFOUTPUT=$dir;export TEXMFOUTPUT; cd $dir; " . AUTOMATTIC_LATEX_LATEX_PATH . " --interaction nonstopmode $this->file_base.tex > /dev/null 2>&1", $out, $r );
+		if ( 0 != $r )
+			return new WP_Error( 'automattic-latex', __( 'Formula does not parse', 'automattic-latex' ) );
+		exec( AUTOMATTIC_LATEX_DVIPNG_PATH . " $this->file_base.dvi -o $this->file_base.png -bg 'rgb $this->bg_rgb' -fg 'rgb $this->fg_rgb' -T tight > /dev/null 2>&1" );
+
+		unlink( "$this->file_base.tex" );
+		unlink( "$this->file_base.aux" );
+		unlink( "$this->file_base.log" );
+		unlink( "$this->file_base.dvi" );
+
+		return "$this->file_base.png";
+	}
+
+        
+	function render_error( $error ) {
+		if ( is_wp_error($error) )
+			$error = $error->get_error_message();
+
+		$width  = 7.3 * strlen($error);
+		$image  = imagecreatetruecolor($width, 18);
+		$yellow = imagecolorallocate($image, 255, 255, 0);
+		$red    = imagecolorallocate($image, 255, 0, 0);
+
+		imagefilledrectangle($image, 0, 0, $width, 20, $yellow);
+		imagestring($image, 3, 4, 2, $error, $red);
+
+		return $image;
+	}
+
+	function wrap( $latex ) {
+		$string  = "\documentclass[12pt]{article}\n";
+		$string .= "\usepackage[latin1]{inputenc}\n";
+//		$string .= "\usepackage{textcomp}\n";
+//		$string .= "\usepackage[T1]{fontenc}\n";
+		$string .= "\usepackage{amsmath}\n";
+		$string .= "\usepackage{amsfonts}\n";
+		$string .= "\usepackage{amssymb}\n";
+		$string .= "\usepackage[mathscr]{eucal}\n";
+		$string .= "\pagestyle{empty}\n";
+		$string .= "\begin{document}\n";
+		if ( $this->size ) $string .= "\begin{{$this->size}}\n";
+		$string .= $latex == '\LaTeX' || $latex == '\TeX' ? $latex : "\$" . $latex . "\$";
+		if ( $this->size ) $string .= "\end{{$this->size}}\n";
+		$string .= "\n\end{document}\n";
+		return $string;
+	}
+
+}
+
+if ( !function_exists('__') ) :
+function __($a) { return $a; }
+endif;
+
+if ( !class_exists('WP_Error') ) :
+class WP_Error {
+	var $e;
+	function WP_Error( $c, $m ) { $this->e = $m; }
+	function get_error_message() { return $this->e; }
+}
+function is_wp_error($a) { return is_object($a) && is_a($a, 'WP_Error'); }
+endif;
+
+?>
---
+++ readme.txt
@@ -0,0 +1,120 @@
+=== WP LaTeX ===
+Contributors: mdawaffe
+Tags: latex, math, equations
+Stable tag: trunk
+Requires at least: 2.1
+Tested up to: 2.2
+
+WP LaTeX creates PNG images from inline $\LaTeX$ code in your posts and comments.
+
+== Description ==
+
+Writing equations and formulae is a snap with LaTeX, but really hard on a website.
+No longer.  This plugin combines the power of LaTeX and the simplicity of WordPress
+to give you the ultimate in math blogging platforms.
+
+Wow that sounds nerdy.
+
+== Installation ==
+
+This plugin requires several external programs to be installed and working on your
+server, so installation is bit complicated.  Many hosts will not be able to
+support this plugin.
+
+= Server Requirements =
+1. Your server must be running some flavor of Linux, UNIX, or BSD.
+2. You must have a working installation of LaTeX running.  I recommend the
+   `tetex-extra` package availabe to most Linux distributions.
+3. `dvipng` needs to be installed as well (easily installed with the `dvipng`
+   package).
+
+= Setup =
+1. Install and activate the [FauxML plugin](http://wordpress.org/extend/plugins/fauxml/).
+2. Create a directory called `/wp-content/latex/` and make it writable by your
+   webserver (chmod 777 will do the trick).
+3. Upload `wp-latex.php` and `automattic-latex.php` to the
+   `/wp-content/plugins/` directory.
+4. Activate the plugin through the 'Plugins' menu in WordPress.
+5. Go to Plugins -> WP LaTeX to configure the plugin.
+
+== Frequently Asked Questions ==
+
+= How do I add LaTeX to my posts? =
+
+The syntax this plugin uses is reminiscent of LaTeX's inline math mode syntax.
+
+If you would have written `$some-code$` in a LaTeX document, just write `$latex some-code$`
+in your WordPress post.
+
+`
+$latex e^{\i \pi} + 1 = 0$
+`
+
+= Can I change the color of the images produced? =
+
+Yes.  You can set the default text color and background color of the images in the
+Plugins -> WP LaTeX admin page.
+
+You can also change the color on an image by image basis by specifying `fg` and `bg`
+parameters after the LaTeX code, respectively.  For example:
+
+`
+$latex e^{\i \pi} + 1 = 0&bg=00ff00&fg=ff0000$
+`
+
+will produce an image with a bright green background and a bright red foreground color.
+Colors are specified in 6 digit hex notation.
+
+= Can I change the size of the image? =
+
+You can specify an `s` parameter after the LaTeX code.
+
+`
+$latex e^{\i \pi} + 1 = 0&s=X$
+`
+
+Where X goes from -4 to 4 (0 is the default).  These numbers correspond to the following
+LaTeX size commands.
+
+	s=	LaTeX size
+	-4	\tiny
+	-3	\scriptsize
+	-2	\footnotesize
+	-1	\small
+	0	\normalsize (12pt)
+	1	\large
+	2	\Large
+	3	\LARGE
+	4	\huge
+
+= I want to break out of math mode and do some really wild stuff.  How do I do that? =
+
+You can't with this plugin.  WP LaTeX forces you to stay in math mode.  Formatting and
+styling for your posts should be done with markup and CSS, not LaTeX.
+
+If you really want hardcore LaTeX formatting (or any other cool LaTeX features), you
+should probably just use LaTeX.
+
+= Instead of images, I get error messages.  What's up =
+
+* `Formula does not parse`: Your LaTeX is invalid; there must be a syntax error or
+  something in your code (WP LaTeX doesn't provide any debugging).
+* `Formula Invalid`: Your LaTeX code attempts to use LaTeX commands that this plugin
+  does not allow for security reasons.
+* `You must stay in inline math mode`: Fairly self explanitory, don't you think?
+  See above.
+* `The forumula is too long`: Break your LaTeX up into multiple images.  WP LaTeX
+  limits you to 2000 characters per image.
+* `Could not open TEX file for writing` or `Could not write to TEX file`: You have
+  some file permissions problems.  See Intallation instructions.
+
+= Do I really need to intsall FauxML for WP LaTeX to work? =
+
+Yes.
+
+== Other Plugins ==
+
+[Steve Mayer's LatexRender Plugin](http://sixthform.info/steve/wordpress/index.php?p=13)
+is based on a [LaTeX Rendering Class](http://www.mayer.dial.pipex.com/tex.htm) originally
+written by Benjamin Zeiss.  It's requirements are somewhat different and has a different 
+installation procedure.
