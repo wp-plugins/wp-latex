593cc7233ec37f17909bf1b5e9a421072eb7d547
mdawaffe <mdawaffe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-12-01 00:49:27 +0000

    constant typos.  Normalize image output sizes across dvipng, dvips, convert
    git-svn-id: https://plugins.svn.wordpress.org/wp-latex/trunk@25842 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- automattic-latex.php
+++ automattic-latex.php
@@ -14,20 +14,20 @@ Inspired in part by:
  * 	AUTOMATTIC_LATEX_CONVERT_PATH and AUTOMATTIC_LATEX_DVIPS_PATH
  * must be defined
  */
-if ( !defined('AUTOMATTIC_LATEX_LATEX_PATH') || !file_exists('AUTOMATTIC_LATEX_LATEX_PATH') )
+if ( !defined('AUTOMATTIC_LATEX_LATEX_PATH') || !file_exists(AUTOMATTIC_LATEX_LATEX_PATH) )
 	return;
 
 if (
-	( !defined('AUTOMATTIC_LATEX_DVIPNG_PATH') || !file_exists('AUTOMATTIC_LATEX_DVIPNG_PATH') )
+	( !defined('AUTOMATTIC_LATEX_DVIPNG_PATH') || !file_exists(AUTOMATTIC_LATEX_DVIPNG_PATH) )
 
 	&&
 
 	(
-		( !defined('AUTOMATTIC_LATEX_DVIPS_PATH') || !file_exists('AUTOMATTIC_LATEX_DVIPS_PATH') )
+		( !defined('AUTOMATTIC_LATEX_DVIPS_PATH') || !file_exists(AUTOMATTIC_LATEX_DVIPS_PATH) )
 
 		||
 
-		( !defined('AUTOMATTIC_LATEX_CONVERT_PATH') || !file_exists('AUTOMATTIC_LATEX_CONVERT_PATH') )
+		( !defined('AUTOMATTIC_LATEX_CONVERT_PATH') || !file_exists(AUTOMATTIC_LATEX_CONVERT_PATH) )
 	)
 )
 	return;
@@ -216,7 +216,7 @@ class Automattic_Latex {
 			}
 		
 			if ( defined( 'AUTOMATTIC_LATEX_DVIPNG_PATH' ) && file_exists(AUTOMATTIC_LATEX_DVIPNG_PATH) ) {
-				$dvipng_exec = AUTOMATTIC_LATEX_DVIPNG_PATH . " $this->tmp_file.dvi -o $png_file -T tight";
+				$dvipng_exec = AUTOMATTIC_LATEX_DVIPNG_PATH . " $this->tmp_file.dvi -o $png_file -T tight -D 100";
 				exec( "$dvipng_exec > /dev/null 2>&1", $dvipng_out, $d );
 				if ( 0 != $d ) {
 					$r = new WP_Error( 'dvipng_exec', __( 'Cannot create image', 'automattic-latex' ), $dvipng_exec );
@@ -225,19 +225,19 @@ class Automattic_Latex {
 				break;
 			}
 
-			if ( ( !defined('AUTOMATTIC_LATEX_DVIPS_PATH') || !file_exists('AUTOMATTIC_LATEX_DVIPS_PATH') ) || ( !defined('AUTOMATTIC_LATEX_CONVERT_PATH') || !file_exists('AUTOMATTIC_LATEX_CONVERT_PATH') ) ) {
+			if ( ( !defined('AUTOMATTIC_LATEX_DVIPS_PATH') || !file_exists(AUTOMATTIC_LATEX_DVIPS_PATH) ) || ( !defined('AUTOMATTIC_LATEX_CONVERT_PATH') || !file_exists(AUTOMATTIC_LATEX_CONVERT_PATH) ) ) {
 				$r = new WP_Error( 'dviping', __( 'Neither dvipng nor dvips and convert are available.', 'automattic-latex' ) );
 				break;
 			}
 
-			$dvips_exec = AUTOMATTIC_LATEX_DVIPS_PATH . " -E $this->tmp_file.dvi -o $this->tmp_file.ps";
+			$dvips_exec = AUTOMATTIC_LATEX_DVIPS_PATH . " -D 100 -E $this->tmp_file.dvi -o $this->tmp_file.ps";
 			exec( "$dvips_exec > /dev/null 2>&1", $dvips_out, $dps );
 			if ( 0 != $dps ) {
 				$r = new WP_Error( 'dvips_exec', __( 'Cannot create image', 'automattic-latex' ), $dvips_exec );
 				break;
 			}
 
-			$convert_exec = AUTOMATTIC_LATEX_CONVERT_PATH . " -units PixelsPerInch -density 72 $this->tmp_file.ps $png_file";
+			$convert_exec = AUTOMATTIC_LATEX_CONVERT_PATH . " -units PixelsPerInch -density 100 $this->tmp_file.ps $png_file";
 			exec( "$convert_exec > /dev/null 2>&1", $convert_out, $c );
 			if ( 0 != $c ) {
 				$r = new WP_Error( 'convert_exec', __( 'Cannot create image', 'automattic-latex' ), $convert_exec );
