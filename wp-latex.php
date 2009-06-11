f8e010f6c6a34f78796aa7b2f07897d8f7e84094
mdawaffe <mdawaffe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-11 18:16:29 +0000

    release 1.5
    git-svn-id: https://plugins.svn.wordpress.org/wp-latex/trunk@124849 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-latex.php
+++ wp-latex.php
@@ -3,7 +3,7 @@
 Plugin Name: WP LaTeX
 Plugin URI: http://automattic.com/code/
 Description: Converts inline latex code into PNG images that are displayed in your blog posts and comments.  Use either [latex]e^{\i \pi} + 1 = 0[/latex] or $latex e^{\i \pi} + 1 = 0$ syntax.
-Version: 1.5-alpha
+Version: 1.5
 Author: Automattic, Inc.
 Author URI: http://automattic.com/
 
